96dc8166cc42babcdec3d6473e600b887a1e4fd6
YamiiiN <159107432+YamiiiN@users.noreply.github.com>
2025-09-04 22:51:30 +0800

    baseUrl

--- screens/user/register.js
+++ screens/user/register.js
@@ -1,10 +1,148 @@
-import React from 'react'
-import { View, Text } from 'react-native'   
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    SafeAreaView,
+    StatusBar,
+    Alert
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+import Toast from 'react-native-toast-message';
+import { useNavigation } from '@react-navigation/native';
+import baseURL from '../../assets/common/baseUrl';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Register() {
-  return (
-    <View>
-        <Text>register</Text>  
-    </View>
-  )
+    const navigation = useNavigation();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [profilePic, setProfilePic] = useState(null);
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+     const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const toggleConfirmPasswordVisibility = () => {
+        setShowConfirmPassword(!showConfirmPassword);
+    };
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setProfilePic(result.assets[0].uri);
+        }
+    };
+
+    const takePhoto = async () => {
+        const { status } = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (status === "granted") {
+            let result = await ImagePicker.launchCameraAsync({
+                aspect: [4, 3],
+                quality: 1,
+            });
+
+            if (!result.canceled) {
+                setProfilePic(result.assets[0].uri);
+            }
+        }
+    };
+
+     const handleRegister = async () => {
+        if (email === "" || firstName === "" || lastName === "" || password === "") {
+            Toast.show({
+                type: 'error',
+                text1: 'Error',
+                text2: 'Please fill in the form correctly',
+            });
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            Toast.show({
+                type: 'error',
+                text1: 'Error',
+                text2: 'Passwords do not match',
+            });
+            return;
+        }
+
+        const formData = new FormData();
+
+        formData.append('firstName', firstName);
+        formData.append('lastName', lastName);
+        formData.append('email', email);
+        formData.append('password', password);
+
+        if (profilePic) {
+            const filename = profilePic.split('/').pop();
+            const match = /\.(\w+)$/.exec(filename ?? '');
+            const type = match ? `profilePic/${match[1]}` : `image`;
+
+            formData.append('profilePic', {
+                uri: profilePic,
+                name: filename,
+                type,
+            });
+        }
+
+        try {
+            const res = await fetch(`${baseURL}/api/user/register`, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            const data = await res.json();
+
+            if (res.ok) {
+                Toast.show({
+                    type: 'success',
+                    text1: 'Success',
+                    text2: data.message || 'Registration successful!',
+                });
+                setTimeout(() => {
+                    navigation.navigate('Login');
+                }, 1500);
+
+            } else {
+                Toast.show({
+                    type: 'error',
+                    text1: 'Error',
+                    text2: data.message || 'Something went wrong.',
+                });
+            }
+        } catch (error) {
+            console.error(error);
+            Toast.show({
+                type: 'error',
+                text1: 'Error',
+                text2: 'Network error or server not reachable.',
+            });
+        }
+    };
+
+
+    return (
+        <View>
+            <Text>register</Text>
+        </View>
+    )
 }
