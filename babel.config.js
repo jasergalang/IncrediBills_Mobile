42b82501657b0687cf88187ac40a296f53de5f06
YamiiiN <159107432+YamiiiN@users.noreply.github.com>
2026-03-23 20:58:22 +0800

    BABEL

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,22 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+//       "nativewind/babel",
+//     ],
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    ],
+    plugins: [
+      "nativewind/babel",                  // 👈 moved to plugins
+      "react-native-reanimated/plugin",    // 👈 add this, must be LAST
     ],
   };
 };
