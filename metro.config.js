b08568b8a295ea9113f3e9523cefeb044e847d3b
YamiiiN <159107432+YamiiiN@users.noreply.github.com>
2026-04-28 19:51:46 +0800

    fix: add browser field for xlsx, update worklets version

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,18 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for packages that use Node.js built-ins
+config.resolver.blockList = [
+  /node_modules\/xlsx\/lib\/nodelist\.js/,
+];
+
+config.resolver.extraNodeModules = {
+  fs: require.resolve("@expo/vector-icons"),
+  crypto: require.resolve("expo-crypto") ,
+  stream: require.resolve("stream-browserify"),
+  path: require.resolve("path-browserify"),
+};
+
 module.exports = withNativeWind(config, {
   input: "./global.css",
 });
