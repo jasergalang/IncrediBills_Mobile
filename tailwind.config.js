80d59d1163db101ce72211c5419939a13f9accad
YamiiiN <159107432+YamiiiN@users.noreply.github.com>
2026-04-29 09:46:20 +0800

    nativewind downgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-@type {import('tailwindcss').Config} 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: [
