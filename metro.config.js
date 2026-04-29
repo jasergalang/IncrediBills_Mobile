e3199578da498c6149c3b9cb27a6d1f4e39921a9
YamiiiN <159107432+YamiiiN@users.noreply.github.com>
2026-04-29 09:28:58 +0800

    nativewind downgrade

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,9 @@
-/*const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 //module.exports = config;
 module.exports = withNativeWind(config, {
   input: "./global.css",
-}); */
+}); 
 
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve("react-native-svg-transformer"),
-};
-
-module.exports = config;
