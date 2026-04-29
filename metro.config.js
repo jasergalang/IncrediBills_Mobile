bbc94ab16680b54559ecc61226fcc23905917e77
YamiiiN <159107432+YamiiiN@users.noreply.github.com>
2026-04-29 09:24:52 +0800

    babel and metro

--- metro.config.js
+++ metro.config.js
@@ -8,13 +8,12 @@ module.exports = withNativeWind(config, {
 }); */
 
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts.push("css");
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
 
-module.exports = withNativeWind(config, {
-  input: "./global.css",
-  inlineRem: 16,
-});
+module.exports = config;
